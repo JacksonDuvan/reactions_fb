af539177ef5fef36c07866236945d6a5df20c88c
JacksonDev <jzambranogarzon@gmail.com>
2020-05-03 17:08:36 -0500

    Bugs

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 3000
 
 const app = express()
 
-app.use('/', express.static('public'))
+app.use(express.static('public'))
 
 app.listen(port, (req, res) => {
     console.log(`Listening in http://localhost:${port}`)
