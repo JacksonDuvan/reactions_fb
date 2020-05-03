d6dd8b3df5bdc86d47f22d7bb200a64ae8a47f29
JacksonDev <jzambranogarzon@gmail.com>
2020-05-03 16:53:00 -0500

    Se creo el npm run start

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 
+const port = 3000
+
 const app = express()
 
 app.use('/', express.static('public'))
 
-app.listen(3000)
-console.log('Listening in http://localhost:3000')
+app.listen(port, (req, res) => {
+    console.log(`Listening in http://localhost:${port}`)
+})
